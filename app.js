7eb205a0e2a9c41315c2e78ea3381e540314dd4e
andre <kurepin.andre@yandex.ru>
2023-12-07 22:32:34 +0300

    Положено начало проекта

---
+++ app.js
@@ -0,0 +1,18 @@
+const path = require('path');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const { PORT = 3000, BASE_PATH } = process.env;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }))
+
+mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+
+app.use('/test', require('./routes/users'))
+
+app.listen(PORT, () => {
+  console.log('WORK');
+});
---
+++ controllers/users.js
@@ -0,0 +1,8 @@
+const User = require('../models/user')
+
+module.exports.createUser = (req, res) => {
+  const { name, about, avatar } = req.body;
+  User.create({ name, about, avatar })
+    .then(user => res.send(user))
+    .catch(err => res.status(500).send({message: err.message}))
+}
