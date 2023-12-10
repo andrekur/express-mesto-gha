076bc6d1d8dec82b1f4e6e0ad90b873ceb764511
andre <kurepin.andre@yandex.ru>
2023-12-10 12:31:42 +0300

    рефакторинг карточек

--- controllers/cards.js
+++ controllers/cards.js
@@ -20,41 +20,29 @@ module.exports.getCards = (req, res) => {
 }
 
 module.exports.deleteCard = (req, res) => {
-  getObjOrError(Card, req.params.cardId)
-    .then(() => {
-      Card.findByIdAndDelete(req.params.cardId)
-        .then(() => res.status(HTTP_STATUS_OK).send({}))
-        .catch(err => APIError(req, res, err))
-    })
+  Card.findByIdAndDelete(req.params.cardId).orFail()
+    .then(() => res.status(HTTP_STATUS_OK).send({}))
     .catch(err => APIError(req, res, err))
 }
 
 module.exports.likeCard = (req, res) => {
-  getObjOrError(Card, req.params.cardId)
-    .then(() => {
-      Card.findByIdAndUpdate(
-        req.params.cardId,
-        { $addToSet: { likes: req.user._id } },
-        { new: true },
-      )
-        .then(card => card.populate(['likes', 'owner']))
-        .then(card => res.send(card))
-        .catch(err => APIError(req, res, err))
-    })
+  Card.findByIdAndUpdate(
+    req.params.cardId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true },
+  ).orFail()
+    .then(card => card.populate(['likes', 'owner']))
+    .then(card => res.send(card))
     .catch(err => APIError(req, res, err))
 }
 
 module.exports.dislikeCard = (req, res) => {
-  getObjOrError(Card, req.params.cardId)
-    .then(() => {
-      Card.findByIdAndUpdate(
-        req.params.cardId,
-        { $pull: { likes: req.user._id } },
-        { new: true },
-      )
-        .then(card => card.populate(['likes', 'owner']))
-        .then(card => res.send(card))
-        .catch(err => APIError(req, res, err))
-    })
+  Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true },
+  ).orFail()
+    .then(card => card.populate(['likes', 'owner']))
+    .then(card => res.send(card))
     .catch(err => APIError(req, res, err))
 }
