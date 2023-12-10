4f0f398a16707cba387fdd3290c0dc45d5909215
andre <kurepin.andre@yandex.ru>
2023-12-10 10:39:50 +0300

    Удаляет body-parser

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
 const { HTTP_STATUS_NOT_FOUND  } = require('http2').constants;
 
 
@@ -9,8 +8,8 @@ const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }))
 app.use((req, res, next) => {
   req.user = {
     _id: '5d8b8592978f8bd833ca8133'
